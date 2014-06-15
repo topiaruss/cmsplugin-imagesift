8539c54e94d53c3b1533fe4feff27c2c68e83242
Russ Ferriday <russf@topia.com>
2014-06-15 21:52:28 +0100

    added debug for the case where sorting fails due to naive dates.

--- imagesift/cms_plugins.py
+++ imagesift/cms_plugins.py
@@ -1,4 +1,5 @@
 import datetime
+import logging
 
 from django.utils.translation import ugettext_lazy as _
 
@@ -6,6 +7,7 @@ from cms.plugin_base import CMSPluginBase
 from cms.plugin_pool import plugin_pool
 from .models import GalleryPlugin
 
+logger = logging.getLogger(__name__)
 
 class ImagesiftPlugin(CMSPluginBase):
     model = GalleryPlugin
@@ -20,6 +22,17 @@ class ImagesiftPlugin(CMSPluginBase):
         for i in images:
             dates.setdefault(i.overrideable_date().date(), None)
         return sorted(dates.keys())
+
+    def sort_by_overrideable_date(self, qs):
+        try:
+            qs.sort(key=lambda i: i.overrideable_date())
+        except TypeError:
+            # poss "can't compare offset-naive and offset-aware datetimes"
+            logger.exception('comparison of naiive/smart dates?')
+            for i in qs:
+                logger.debug('overridable_date : %s' % i.overrideable_date().isoformat())
+        return qs
+
 
     def render(self, context, instance, placeholder):
         url = context['request'].get_full_path()
@@ -36,7 +49,7 @@ class ImagesiftPlugin(CMSPluginBase):
             filtered = _('The set of images is filtered to %s' % unicode(date))
 
         # sort before limit
-        qs.sort(key=lambda i: i.overrideable_date())
+        qs = self.sort_by_overrideable_date(qs)
 
         if limit:
             qs = qs[:limit]
