b7e33bc26b190a559026e285b6c77f344a2c6fdb
Russ Ferriday <russf@topia.com>
2014-07-03 19:51:00 +0100

    Fix the silly drop down flickering. Django CMS caches by default!

--- imagesift/cms_plugins.py
+++ imagesift/cms_plugins.py
@@ -14,6 +14,7 @@ class ImagesiftPlugin(CMSPluginBase):
     model = GalleryPlugin
     name = _('Imagesift Plugin')
     render_template = "imagesift_plugin.html"
+    cache = False       # Important because we change response depending on the request, not just the instance.
 
     def date_digest(self, images):
         """
